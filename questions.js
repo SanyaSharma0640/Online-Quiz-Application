cb4cda1ab3b1fdc54dc163af657f4c9ea2d4c363
SanyaSharma0640 <sanya0640.be20@chitkara.edu.in>
2022-09-27 22:56:19 +0530

    first commit

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Javascript is an ________ language?",
+    answer: "Object_Oriented",
+    options: [
+      "Object-Based",
+      "Procedural",
+      "Object-Oriented",
+      "None of these"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Both A and B",
+    options: [
+      "getElementById()",
+      "getElementByClassName()",
+      "Both A and B",
+      "None of these"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following methods is used to to access HTML elements using Javascript?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+    answer: "Ignores the statements",
+    options: [
+      "Throws an Erroe",
+      "Ignores the statements",
+      "Gives a warning",
+      "None of these"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
